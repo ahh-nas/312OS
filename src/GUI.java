e99ca323c59f4c8945ff455bc77bdaad01f55494
AnasZafar <zafarma2@vcu.edu>
2017-11-27 20:45:01 -0500

    REgression Of our GUI to meet min. req.

--- src/GUI.java
+++ src/GUI.java
@@ -13,12 +13,14 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.*;
 
 public class GUI extends JFrame {
-
+int a =0;
 	private JPanel contentPane;
 	private JTextField txtEnterCommand;
-	private JTable table;
+	
 
 	/**
 	 * Launch the application.
@@ -46,81 +48,31 @@ public class GUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JButton btnRoundRobin = new JButton("Round Robin");
-		btnRoundRobin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		JButton btnFirstComeFirst = new JButton("First Come First Serve");
-		btnFirstComeFirst.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		JButton btnShortestJobFirst = new JButton("Shortest Job First");
-		btnShortestJobFirst.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		JLabel lblSelectAScheduler = new JLabel("Select A Scheduler");
-		
-		JButton btnReset = new JButton("Reset");
-		
 		txtEnterCommand = new JTextField();
-		txtEnterCommand.setText("Enter Command");
-		txtEnterCommand.setColumns(10);
 		
-		table = new JTable();
+		txtEnterCommand.setColumns(10);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addContainerGap()
-									.addComponent(lblSelectAScheduler))
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(btnFirstComeFirst, GroupLayout.PREFERRED_SIZE, 169, Short.MAX_VALUE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(btnRoundRobin, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE)))
-							.addPreferredGap(ComponentPlacement.UNRELATED))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(btnReset)
-							.addGap(209)))
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(txtEnterCommand, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-							.addComponent(btnShortestJobFirst)
-							.addGap(19))))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(51, Short.MAX_VALUE)
-					.addComponent(table, GroupLayout.PREFERRED_SIZE, 365, GroupLayout.PREFERRED_SIZE)
-					.addGap(48))
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(txtEnterCommand, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(300, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(15)
-					.addComponent(lblSelectAScheduler)
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnFirstComeFirst)
-						.addComponent(btnShortestJobFirst)
-						.addComponent(btnRoundRobin))
-					.addGap(18)
-					.addComponent(table, GroupLayout.PREFERRED_SIZE, 111, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnReset)
-						.addComponent(txtEnterCommand, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(234, Short.MAX_VALUE)
+					.addComponent(txtEnterCommand, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
 		contentPane.setLayout(gl_contentPane);
+
+
+	}
+	public static int getValue(int a)
+	{
+		return a;
+
 	}
 }
